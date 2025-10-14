2d41384852d11781127da364382d1f6f3def91fe
autisticboy348 <gospodnikov9roman@gmail.com>
2025-10-14 17:07:49 +0300

    homework.cs

---
+++ Homework/Homework/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Your number:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Your second number:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Your sign:");
+char sign = Convert.ToChar(Console.ReadLine());
+if (sign == '+')
+{
+    Console.WriteLine(num1 + num2);
+}
+else if (sign == '-')
+{
+    Console.WriteLine(num1 - num2);
+}
+else if (sign == '/')
+{
+    Console.WriteLine(num1 / num2);
+}
+else if (sign == '*')
+{
+    if (num2 == 0)
+    {
+        Console.WriteLine("Error");
+    }
+    else
+    {
+        Console.WriteLine(num1 * num2);
+    }
+}
+else
+{
+    Console.WriteLine("Error");
+}
